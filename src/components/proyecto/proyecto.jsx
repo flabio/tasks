fbe7896c2a3922d8e68623d71e6dd55069f19383
flabiohs <flabiohs@gmail.com>
2021-02-04 09:39:16 -0500

    components proyecto

---
+++ src/components/proyecto/proyecto.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const proyecto = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+proyecto.propTypes = {
+    
+};
+
+export default proyecto;
